2ca5cc42e28e17170449413ae4ec6ef13723c9f2
The_Number_e <edwardx999@gmail.com>
2022-06-14 18:28:12 -0400

    add more

--- src/js/data/2022-05-21.js
+++ src/js/data/2022-05-21.js
@@ -14,12 +14,13 @@ dataSet[dataSetVersion].options = [
     name: "Filter by Period",
     key: "period",
     tooltip: "Doesn't really make sense but ¯\\_(ツ)_/¯",
-    checked: true,
+    checked: false,
     sub: [
       { name: "Medieval", tooltip: "⚔️", key: "medieval" },
-      { name: "Baroque", key: "baroque" },
+      { name: "Renaissance", key: "renaissance" },
+      { name: "Baroque", tooltip: "☀️", key: "baroque" },
       { name: "Classical", tooltip: "💤", key: "classical" },
-      { name: "Romantic", tooltip: "🤮", key: "romantic" },
+      { name: "Romantic", tooltip: "🤮 (usually)", key: "romantic" },
       { name: "Modern", tooltip: "first half of 20th century(???)", key: "modern" },
       { name: "Contemporary", tooltip: "As if this actually means anything", key: "contemporary" }
     ]
@@ -30,15 +31,19 @@ dataSet[dataSetVersion].options = [
     tooltip: "Check this to restrict to certain countries.",
     checked: false,
     sub: [
+      { name: "Austria", key: "at" },
+      { name: "Azerbaijan", key: "az" },
       { name: "China (People's Republic)", tooltip: "great firewall land", key: "cn" },
       { name: "France", tooltip: "baguette land", key: "fr" },
       { name: "Germany", tooltip: "bier land", key: "de" },
+      { name: "Hungary", key: "hu" },
+      { name: "Italy", tooltip: "spaghetti land", key: "it" },
       { name: "Japan", tooltip: "anime land", key: "jp" },
       { name: "Korea (South and North)", key: "kr" },
-      { name: "Russia", key: "ru" },
       { name: "Netherlands", tooltip: "windmill land", key: "nl" },
+      { name: "Russia", key: "ru" },
       { name: "UK", tooltip: "tea land", key: "uk" },
-      { name: "USA", tooltip: "gun land", key: "us" },
+      { name: "USA", tooltip: "gun land", key: "us" }
     ]
   },
   {
@@ -68,7 +73,7 @@ dataSet[dataSetVersion].options = [
   {
     name: "Only good composers",
     key: "bad",
-    checked: false,
+    checked: true,
     tooltip: "Completely objective and unbiased"
   }
 ]
@@ -79,7 +84,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Louis Andriessen",
     img: "https://i.imgur.com/ZpXLo2o.jpg",
     opts: {
-      period: [ "contemporary" ],
+      period: ["contemporary"],
       country: ["nl"],
       continent: ["eu"],
       genre: ["classical"]
@@ -90,7 +95,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Olivier Messiaen",
     img: "https://i.imgur.com/uNlLCR6.jpg",
     opts: {
-      period: [ "modern", "contemporary" ],
+      period: ["modern", "contemporary"],
       country: ["fr"],
       continent: ["eu"],
       genre: ["classical"]
@@ -100,7 +105,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Pierre Boulez",
     img: "https://i.imgur.com/4H7hOND.jpg",
     opts: {
-      period: [ "contemporary"],
+      period: ["contemporary"],
       country: ["fr"],
       continent: ["eu"],
       genre: ["classical"]
@@ -110,7 +115,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Germaine Tailleferre",
     img: "https://i.imgur.com/dvXTV55.jpg",
     opts: {
-      period: [ "modern"],
+      period: ["modern"],
       country: ["fr"],
       continent: ["eu"],
       genre: ["classical"]
@@ -120,8 +125,151 @@ dataSet[dataSetVersion].characterData = [
     name: "Gérard Grisey",
     img: "https://i.imgur.com/6SMcJp5.jpg",
     opts: {
-      period: [ "contemporary"],
+      period: ["contemporary"],
       country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Claude Debussy",
+    img: "https://i.imgur.com/Ega7VLe.png",
+    opts: {
+      period: ["modern"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Maurice Ravel",
+    img: "https://i.imgur.com/uOz5Cib.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Mel Bonis",
+    img: "https://i.imgur.com/wBqLm6U.png",
+    opts: {
+      period: ["romantic"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "François Couperin",
+    img: "https://i.imgur.com/9osIUUA.png",
+    opts: {
+      period: ["baroque"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Claude LeJeune",
+    img: "https://i.imgur.com/9BdOEWj.png",
+    opts: {
+      period: ["renaissance"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Guillaume de Machaut",
+    img: "https://i.imgur.com/QjcOqWg.jpg",
+    opts: {
+      period: ["medieval"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Jehan Alain",
+    img: "https://i.imgur.com/Q0Sdhd2.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Sébastien de Brossard",
+    img: "https://i.imgur.com/SkO1yRv.png",
+    opts: {
+      period: ["baroque"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Lili Boulanger",
+    img: "https://i.imgur.com/FxBHKVo.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Nadia Boulanger",
+    img: "https://i.imgur.com/Peh38yj.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["fr"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  // Germany
+  {
+    name: "J.S. Bach",
+    img: "https://i.imgur.com/RSBPfqz.jpg",
+    opts: {
+      period: ["baroque"],
+      country: ["de"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  // Hungary
+  {
+    name: "Béla Bartók",
+    img: "https://i.imgur.com/bdwMRrA.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["hu"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  // UK
+  {
+    name: "Gustav Holst",
+    img: "https://i.imgur.com/BbWoFRX.png",
+    opts: {
+      period: ["modern"],
+      country: ["uk"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Frederick Delius",
+    img: "https://i.imgur.com/zFbYU7P.png",
+    opts: {
+      period: ["modern"],
+      country: ["uk"],
       continent: ["eu"],
       genre: ["classical"]
     }
@@ -131,9 +279,124 @@ dataSet[dataSetVersion].characterData = [
     name: "John Adams",
     img: "https://i.imgur.com/1t3FsRu.jpg",
     opts: {
-      period: [ "classical"],
+      period: ["contemporary"],
       country: ["us"],
       continent: ["na"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Henry Cowell",
+    img: "https://i.imgur.com/eFv7ned.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["us"],
+      continent: ["na"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Aaron Copland",
+    img: "https://i.imgur.com/FZhwlKD.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["us"],
+      continent: ["na"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "John Cage",
+    img: "https://i.imgur.com/j4sIvSn.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["us"],
+      continent: ["na"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Leonard Bernstein",
+    img: "https://i.imgur.com/PAmiUSF.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["us"],
+      continent: ["na"],
+      genre: ["classical"]
+    }
+  },
+  // Korea
+  {
+    name: "Unsuk Chin",
+    img: "https://i.imgur.com/MLip8Ov.png",
+    opts: {
+      period: ["contemporary"],
+      country: ["kr"],
+      continent: ["asia"],
+      genre: ["classical"]
+    }
+  },
+  // Austria
+  {
+    name: "Alban Berg",
+    img: "https://i.imgur.com/98R9lTc.jpg",
+    opts: {
+      period: ["modern"],
+      country: ["at"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  // China
+  {
+    name: "Chen Xiaoyong",
+    img: "https://i.imgur.com/Y3I8fSK.png",
+    opts: {
+      period: ["contemporary"],
+      country: ["cn"],
+      continent: ["asia"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Chen Gang",
+    img: "https://i.imgur.com/TsePZiQ.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["cn"],
+      continent: ["asia"],
+      genre: ["classical"]
+    }
+  },
+  {
+    name: "Chen Qigang",
+    img: "https://i.imgur.com/VXxrON1.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["cn"],
+      continent: ["asia"],
+      genre: ["classical"]
+    }
+  },
+  // Italy
+  {
+    name: "Luciano Berio",
+    img: "https://i.imgur.com/5WQYKEx.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["it"],
+      continent: ["eu"],
+      genre: ["classical"]
+    }
+  },
+  // Azerbaijan
+  {
+    name: "Franghiz Ali-Zadeh",
+    img: "https://i.imgur.com/qtbPiJo.jpg",
+    opts: {
+      period: ["contemporary"],
+      country: ["az"],
+      continent: ["eu"],
       genre: ["classical"]
     }
   },
@@ -142,7 +405,7 @@ dataSet[dataSetVersion].characterData = [
     name: "Nikolai Kapustin",
     img: "https://i.imgur.com/2lQpsf4.jpg",
     opts: {
-      period: [ "contemporary" ],
+      period: ["contemporary"],
       country: ["ru"],
       continent: ["eu"],
       genre: ["jazz"],
